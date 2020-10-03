347d9bf2aa12a6273dbc9803a794c3896acda0c1
DESKTOP-325D3AA\CAMT <valentine_d@cmu.ac.th>
2020-10-03 11:56:19 +0700

    Ver.2

---
+++ 951323_163Midterm_612110085/Assets/_codes/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeStart = 30;
+    public Text textTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textTime.text = timeStart.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart -= Time.deltaTime;
+        textTime.text = Mathf.Round(timeStart).ToString();
+    }
+}
