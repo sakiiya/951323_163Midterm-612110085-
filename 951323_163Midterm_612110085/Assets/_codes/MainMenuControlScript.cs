4348ca56e61812512ef70bc8cad0f499e4933298
DESKTOP-325D3AA\CAMT <valentine_d@cmu.ac.th>
2020-10-03 11:18:47 +0700

    Ver.2

---
+++ 951323_163Midterm_612110085/Assets/_codes/MainMenuControlScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuControlScript : MonoBehaviour
+{
+    //Declarationssection
+    [SerializeField] Button _StageSelectButton;
+    [SerializeField] Button _optionsButton;
+    [SerializeField] Button _exitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _StageSelectButton.onClick.AddListener(delegate { StageSelectButtonClick(_StageSelectButton); });
+        _optionsButton.onClick.AddListener(delegate { OptionsButtonClick(_optionsButton); });
+        _exitButton.onClick.AddListener(delegate { ExitButtonClick(_exitButton); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void StageSelectButtonClick(Button button)
+    {
+        SceneManager.LoadScene("SceneStageSelect");
+    }
+
+    public void OptionsButtonClick(Button button)
+    {
+        if (!SingletonGameApplicationManager.Instance.IsOptionMenuActive)
+        {
+            SceneManager.LoadScene("SceneOptions", LoadSceneMode.Additive);
+            SingletonGameApplicationManager.Instance.IsOptionMenuActive = true;
+        }
+    }
+
+    public void CreditsButtonClicา(Button button)
+    {
+        SceneManager.LoadScene("SceneCredits");
+    }
+
+    public void ExitButtonClick(Button button)
+    {
+        Application.Quit();
+    }
+}
---
+++ 951323_163Midterm_612110085/Assets/_codes/StageSelectControlScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StageSelectControlScript : MonoBehaviour
+{
+    [SerializeField] Button _stageButton1;
+    [SerializeField] Button _stageButton2;
+    [SerializeField] Button _backButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _backButton.onClick.AddListener(delegate { BackToMainMenuButtonClick(_backButton); });
+        _stageButton1.onClick.AddListener(delegate { StageOneButtonClick(_stageButton1); });
+        _stageButton2.onClick.AddListener(delegate { StageTwoButtonClick(_stageButton2); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void StageOneButtonClick(Button button)
+    {
+        SceneManager.UnloadSceneAsync("SceneStageSelect");
+        SceneManager.LoadScene("SceneGameplay");
+        SingletonGameManager.Instance.GameScore++;
+    }
+
+    public void StageTwoButtonClick(Button button)
+    {
+        SceneManager.UnloadSceneAsync("SceneStageSelect");
+        SceneManager.LoadScene("SceneGameplay2");
+        SingletonGameManager.Instance.GameScore++;
+    }
+
+    public void BackToMainMenuButtonClick(Button button)
+    {
+        SceneManager.UnloadSceneAsync("SceneStageSelect");
+        SceneManager.LoadScene("SceneMainMenu");
+        SingletonGameManager.Instance.GameScore++;
+    }
+}
